e44a8b1486f908bda998b2db7fb2b9259d884016
Alvin Goh <alvingohacyg@gmail.com>
2026-03-05 14:10:49 -0700

    Test pass for countCities

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -20,4 +20,8 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    public int countCities() {
+        return cities.size();
+    }
 }
